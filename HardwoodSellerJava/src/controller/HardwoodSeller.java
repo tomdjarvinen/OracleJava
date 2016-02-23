005b7d4da46c1c0ad57c0bb1361d44b2b1c0cdd0
tomdjarvinen <tomdjarvinen@gmail.com>
2016-02-23 02:04:43 -0600

    Add attribute woodTypes to HardwoodSeller class
    Created a static List<woodItem> to hold the price and delivery
    values for all products sold.

--- HardwoodSellerJava/src/controller/HardwoodSeller.java
+++ HardwoodSellerJava/src/controller/HardwoodSeller.java
@@ -3,17 +3,29 @@
  */
 package controller;
 
+
+import java.util.List;
+
+import model.WoodItem;
+
 /**
  * @author Esteban
  *
  */
 public class HardwoodSeller {
 
+	//list holds delivery times and prices for all wood types sold
+	public static List<WoodItem> woodTypes;
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		woodTypes.add(new WoodItem("Cherry", 2.5, 5.95));
+		woodTypes.add(new WoodItem("Curly Maple", 1.5, 6.00));
+		woodTypes.add(new WoodItem("Genuine Mahogany", 3.0, 9.60));
+		woodTypes.add(new WoodItem("Wenge", 5., 22.35));
+		woodTypes.add(new WoodItem("White Oak", 2.3, 6.70));
+		woodTypes.add(new WoodItem("Sawdust", 1., 1.5));
 
 	}
 	
