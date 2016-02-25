b21bcb2b25dcda2ab64f66b14419f15c876187e0
tomdjarvinen <tomdjarvinen@gmail.com>
2016-02-24 19:09:49 -0600

    Calculate delivery time of order
    Added matchType() method, so WoodItem objects can be matched with
    the appropriate WoodOrder information such as price and cost.
    Added deliveryTime method which takes a ArrayList<WoodOrders>
    and returns an estimated delivery time.

--- HardwoodSellerJava/src/controller/HardwoodSeller.java
+++ HardwoodSellerJava/src/controller/HardwoodSeller.java
@@ -30,14 +30,14 @@ public class HardwoodSeller {
 		woodTypes.add(new WoodItem("Wenge", 5., 22.35));
 		woodTypes.add(new WoodItem("White Oak", 2.3, 6.70));
 		woodTypes.add(new WoodItem("Sawdust", 1., 1.5));
-		/*
+		
 		try {
 			readInputFile("testOrder.txt");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+		
 	}
 	
 	public static void readInputFile(String inputFilePath) throws FileNotFoundException{
@@ -46,18 +46,45 @@ public class HardwoodSeller {
 		String name = input.next();
 		String address = input.next();
 		String date = input.nextLine();
-		System.out.println(name + address + date);
-		List<WoodOrder> orders = new ArrayList<WoodOrder>();
+	
+		ArrayList<WoodOrder> orders = new ArrayList<WoodOrder>();
 		while(input.hasNext())
 		{
 			input.useDelimiter("\\p{Punct}");
 			orders.add(new WoodOrder(input.next(), input.nextInt()));
 		}
+		double delivery = deliveryTime(orders);
+		System.out.print(delivery);
 	}
-	
-	public Double deliveryTime(){
+	//returns estimated delivery time for a set of items to be ordered.
+	public static Double deliveryTime(ArrayList<WoodOrder> items){
 		Double deliveryETA = 0.0;
+		for(int i = 0; i < items.size();i++ )
+		{
+			if(items.get(i).getFeet() >=500)
+			{
+				deliveryETA = Math.max(deliveryETA, 5.5*
+						woodTypes.get(matchType(items.get(i))).getBaseDeliveryTime());
+			}
+			else
+			{
+				deliveryETA = Math.max(deliveryETA, 
+						(1+items.get(i).getFeet()/100)*woodTypes.get(matchType(items.get(i))).getBaseDeliveryTime());
+			}
+		}
 		return deliveryETA;
+	}
+	public static int matchType(WoodOrder order)
+	{
+		for(int i=0; i < woodTypes.size(); i++)
+		{
+			System.out.println(order.getType() + ":"+ woodTypes.get(i).getType());
+			if(order.getType().equalsIgnoreCase(woodTypes.get(i).getType()));
+			{
+				return i;
+			}	
+		}
+		return -1;
 	}
 	
 }
